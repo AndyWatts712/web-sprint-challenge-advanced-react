f28d81b1e5854e8af56a139daf4f4bbc32effb91
megawatts68 <andrew.f.watts@gmail.com>
2020-08-08 01:54:41 +0700

    Tests work.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,32 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
-    render(<App />)
+    render(<CheckoutForm />)
 });
 
-test("form shows success message on submit with form details", () => {
-    render(<App />)
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />)
     const fName = screen.getByLabelText(/first/i)
     const lName = screen.getByLabelText(/last/i)
-    const email = screen.getByLabelText(/address/i)
-    const email = screen.getByLabelText(/city/i)
-    const email = screen.getByLabelText(/state/i)
-    const email = screen.getByLabelText(/zipi/)
+    const address = screen.getByLabelText(/address/i)
+    const city = screen.getByLabelText(/city/i)
+    const state = screen.getByLabelText(/state/i)
+    const zip = screen.getByLabelText(/zip/i)
 
-    fireEvent.change(fName, { target: { value: 'Ed' } })
-    fireEvent.change(lName, { target: { value: 'Bull' } })
-    fireEvent.change(email, { target: { value: '123 Main' } })
-    fireEvent.change(email, { target: { value: 'Alexandria' } })
-    fireEvent.change(email, { target: { value: 'VA' } })
-    fireEvent.change(email, { target: { value: '22193' } })
+    await fireEvent.change(fName, { target: { value: 'Ed' } })
+    await fireEvent.change(lName, { target: { value: 'Bull' } })
+    await fireEvent.change(address, { target: { value: '123 Main' } })
+    await fireEvent.change(city, { target: { value: 'Alexandria' } })
+    await fireEvent.change(state, { target: { value: 'VA' } })
+    await fireEvent.change(zip, { target: { value: '22193' } })
 
-    const submitBtn = screen.getByRole('submit', { type: 'submit' })
+    const submitBtn = screen.getByText('Checkout')
 
-    fireEvent.click(submitBtn)
-
-    expect(screen.getByText('Ed')).toBeInTheDocument();
+    await fireEvent.click(submitBtn)
+    const success = screen.getByTestId('successMessage')
+    expect(success).toBeInTheDocument();
 });
