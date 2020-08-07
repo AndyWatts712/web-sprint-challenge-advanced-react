b81adb4ce549364a6fea99f300141a82aa89dbd1
megawatts68 <andrew.f.watts@gmail.com>
2020-08-07 23:28:57 +0700

    Form works, somehow.

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,11 @@
 // write your custom hook here to control your checkout form
+const useForm = () => {
+    const values = {}
+
+    const handleChanges = (e) => {
+        const {name, value} = e.target
+        
+    }
+    return [values, handleChanges]
+}
+export default useForm;
